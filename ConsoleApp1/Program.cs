863423ac745b69a5b309ff13a44b43082051928f
Raveli <miguelraveli123@gmail.com>
2024-09-12 00:46:57 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿string dia_30 = "Este mes tem 30 dias";
+string dia_28 = "Este mes tem 28 dias";
+string dia_31 = "Este mes tem 31 dias";
+Console.WriteLine("Digite um mês:");
+string mes = Convert.ToString(Console.ReadLine().ToUpper());
+
+switch (mes)
+{
+    case "FEVEREIRO":
+        Console.WriteLine(dia_28);
+        break;
+    case "JANEIRO":
+    case "MARÇO":
+        Console.WriteLine(dia_31);
+        break;
+    case "ABRIL":
+        Console.WriteLine(dia_30);
+        break;
+    default:
+        Console.WriteLine("Mes Invalido");
+        break;
+}
